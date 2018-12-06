3c3680e64edc90fc7c7a8e91656f4e2391fd7f93
Craig Morgan Roberts <craigmroberts@me.com>
2018-12-06 15:12:29 +0000

    close #14 - removed game stats from view

--- public/views/game/tictactoe/game.php
+++ public/views/game/tictactoe/game.php
@@ -16,26 +16,5 @@
       <div class="square"><div></div></div>
     </div>
 
-    <div class="board-stats">
-      <div class="col player">
-        <span class="title">{{Player}}<br/>(<strong>x</strong>)</span>
-        <span class="score" data-dynamic="totalWon"></span>
-      </div>
-
-      <div class="col tie">
-        <span class="title">{{Tie}}<br/>&nbsp;</span>
-        <span class="score" data-dynamic="totalTies"></span>
-      </div>
-
-      <div class="col computer">
-        <span class="title">{{Computer}}<br/>(<strong>o</strong>)</span>
-        <span class="score" data-dynamic="totalLost"></span>
-      </div>
-
-      <div class="col best-time hidden">
-        <span class="title">{{Best Time}}</span>
-        <div class="best-score"><span class="score" data-dynamic="bestScore"></span><span class="seconds">{{Seconds}}</span></div>
-      </div>
-    </div>
   </div>
 </div>
